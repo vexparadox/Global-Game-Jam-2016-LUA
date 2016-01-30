69672fc30092ba8b4a21eb84213ff425c8d6533e
William Meaton <will.meaton@gmail.com>
2016-01-30 01:53:34 +0000

    added entity data to a static subset

--- entity.lua
+++ entity.lua
@@ -1,38 +1,40 @@
 entity = {
-    player = {
-        sprite = love.graphics.newImage("data/image/player.png"),
-        width = false,
-        height = false,
-        state = false,
-    },
-    -- enemy = {
-    --     sprite = love.graphics.newImage("data/enemy.png"),
-    --     width = false,
-    --     height = false,
-    --     state = false,
-    -- },
-    tree = {
-        sprite = love.graphics.newImage("data/image/orangetree.png"),
-        width = false,
-        height = false,
-        state = false,
-    },
-    -- tile2 = {
-    --     sprite = love.graphics.newImage("data/tile2.png"),
-    --     width = false,
-    --     height = false,
-    --     state = false,
-    -- },
-    -- tile3 = {
-    --     sprite = love.graphics.newImage("data/tile3.png"),
-    --     width = false,
-    --     height = false,
-    --     state = false,
-    -- },
+    static = {
+        player = {
+            sprite = love.graphics.newImage("data/image/player.png"),
+            width = false,
+            height = false,
+            state = false,
+        },
+        -- enemy = {
+        --     sprite = love.graphics.newImage("data/enemy.png"),
+        --     width = false,
+        --     height = false,
+        --     state = false,
+        -- },
+        tree = {
+            sprite = love.graphics.newImage("data/image/orangetree.png"),
+            width = false,
+            height = false,
+            state = false,
+        },
+        -- tile2 = {
+        --     sprite = love.graphics.newImage("data/tile2.png"),
+        --     width = false,
+        --     height = false,
+        --     state = false,
+        -- },
+        -- tile3 = {
+        --     sprite = love.graphics.newImage("data/tile3.png"),
+        --     width = false,
+        --     height = false,
+        --     state = false,
+        -- },
+    }
 }
 
 function entity.load()
-    for _, e in pairs(entity) do
+    for _, e in pairs(entity.static) do
         e.width, e.height = e.sprite:getDimensions()
     end
 end
