0f45b237daf6068db45785ca310f8f68161ea31e
William Meaton <will.meaton@gmail.com>
2016-01-30 01:24:34 +0000

    filled in entities and created draw method

--- entity.lua
+++ entity.lua
@@ -1,5 +1,36 @@
 entity = {
-	player = {
-		sprite = love.graphics.newImage("data/player.png")
-	}
+    player = {
+        sprite = love.graphics.newImage("data/player.png"),
+        width = false,
+        height = false,
+        state = false,
+    },
+    enemy = {
+        sprite = love.graphics.newImage("data/enemy.png"),
+        width = false,
+        height = false,
+        state = false,
+    },
+    tile1 = {
+        sprite = love.graphics.newImage("data/tile1.png"),
+        width = false,
+        height = false,
+        state = false,
+    },
+    tile2 = {
+        sprite = love.graphics.newImage("data/tile2.png"),
+        width = false,
+        height = false,
+        state = false,
+    },
+    tile3 = {
+        sprite = love.graphics.newImage("data/tile3.png"),
+        width = false,
+        height = false,
+        state = false,
+    },
 }
+
+function entity.draw(entity)
+    
+end
