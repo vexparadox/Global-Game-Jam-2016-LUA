3abf4626cdb4fab3544f4e0e02758c75989ad0a0
Benedict Henshaw <benedicthenshaw@gmail.com>
2016-01-30 09:36:09 +0000

    fixed syntax error in collision code

--- collision.lua
+++ collision.lua
@@ -7,20 +7,20 @@ function collision.test(px, py, x1, y1, x2, y2)
            py < y2
 end
 
--- function collision.checkhorizontal(e, lines, dx)
---     for _, line in ipairs(lines) do
---         if dx > line.x and e.x < line.x then do
---             dx = line.x
---         end
---         return dx
---     end
--- end
+function collision.checkhorizontal(e, lines, dx)
+    for _, line in ipairs(lines) do
+        if dx > line.x and e.x < line.x then
+            dx = line.x
+        end
+        return dx
+    end
+end
 
--- function collision.checkvertical(e, lines, dy)
---     for _, line in ipairs(lines) do
---         if dy > line.y and e.y < line.y then do
---             dy = line.y
---         end
---         return dy
---     end
--- end
+function collision.checkvertical(e, lines, dy)
+    for _, line in ipairs(lines) do
+        if dy > line.y and e.y < line.y then
+            dy = line.y
+        end
+        return dy
+    end
+end
